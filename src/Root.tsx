454aead22fac16c40b4f47ffbc4329760da38203
Crystian Emmanuel Reyes Chacón <emmaxing51@gmail.com>
2026-04-05 13:35:03 -0600

    Add language selector widget

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import { AnimalProvider } from "./contexts/AnimalContext";
 import { SiteProvider } from "./contexts/SiteContext";
+import { LanguageWidget } from "./components/LanguageWidget";
 
 export default function Root() {
   return (
     <SiteProvider>
       <AnimalProvider>
         <Outlet />
+        <LanguageWidget />
       </AnimalProvider>
     </SiteProvider>
   );
